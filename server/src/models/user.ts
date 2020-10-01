3c6301fec6791e02d06d7421889c3ff82dbb8954
Chris Cole <cccole3@gmail.com>
2020-10-01 00:23:39 -0500

    Revert "breaking things"
    This reverts commit bee2b8b4a2cc8834a54c06cd00665f3357c951bc.

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,107 +1,37 @@
-import bcrypt from "bcrypt";
-import crypto from "crypto";
 import mongoose from "mongoose";
+import uniqueValidator from "mongoose-unique-validator";
 
-export type UserDocument = mongoose.Document & {
+export interface IUser extends mongoose.Document {
+  name: string;
   email: string;
   password: string;
-  passwordResetToken: string;
-  passwordResetExpires: Date;
-
-  facebook: string;
-  tokens: AuthToken[];
-
-  profile: {
-    name: string;
-    gender: string;
-    location: string;
-    website: string;
-    picture: string;
-  };
-
-  comparePassword: comparePasswordFunction;
-  gravatar: (size: number) => string;
-};
-
-type comparePasswordFunction = (
-  candidatePassword: string,
-  cb: (err: any, isMatch: any) => {}
-) => void;
-
-export interface AuthToken {
-  accessToken: string;
-  kind: string;
+  avatar: string;
+  createDate: Date;
 }
 
-const userSchema = new mongoose.Schema(
-  {
-    email: { type: String, unique: true },
-    password: String,
-    passwordResetToken: String,
-    passwordResetExpires: Date,
-
-    facebook: String,
-    twitter: String,
-    google: String,
-    tokens: Array,
-
-    profile: {
-      name: String,
-      gender: String,
-      location: String,
-      website: String,
-      picture: String
-    }
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
   },
-  { timestamps: true }
-);
-
-/**
- * Password hash middleware.
- */
-userSchema.pre("save", function save(next) {
-  const user = this as UserDocument;
-  if (!user.isModified("password")) {
-    return next();
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  createDate: {
+    type: Date,
+    default: Date.now
   }
-  bcrypt.genSalt(10, (err, salt) => {
-    if (err) {
-      return next(err);
-    }
-    bcrypt.hash(user.password, salt, undefined, (err: mongoose.Error, hash) => {
-      if (err) {
-        return next(err);
-      }
-      user.password = hash;
-      next();
-    });
-  });
 });
 
-const comparePassword: comparePasswordFunction = function (
-  candidatePassword,
-  cb
-) {
-  bcrypt.compare(
-    candidatePassword,
-    this.password,
-    (err: mongoose.Error, isMatch: boolean) => {
-      cb(err, isMatch);
-    }
-  );
-};
+userSchema.plugin(uniqueValidator);
 
-userSchema.methods.comparePassword = comparePassword;
-
-/**
- * Helper method for getting user's gravatar.
- */
-userSchema.methods.gravatar = function (size: number = 200) {
-  if (!this.email) {
-    return `https://gravatar.com/avatar/?s=${size}&d=retro`;
-  }
-  const md5 = crypto.createHash("md5").update(this.email).digest("hex");
-  return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`;
-};
-
-export const User = mongoose.model<UserDocument>("User", userSchema);
+export const User = mongoose.model<IUser>("User", userSchema);
